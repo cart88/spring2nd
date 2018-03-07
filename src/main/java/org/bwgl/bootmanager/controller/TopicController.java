33ad6fae3bd7ff95010b6326013763a6a9ac32e5
Your Name <you@example.com>
2018-03-07 09:31:07 +0800

    添加第二个Controller返回List自动转化成json格式

---
+++ src/main/java/org/bwgl/bootmanager/controller/TopicController.java
@@ -0,0 +1,23 @@
+package org.bwgl.bootmanager.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+	/**
+	 * http://localhost:8080/topics
+	 * @return
+	 */
+	@RequestMapping("/topics")
+	public List<Topic> getAllTopics(){
+		return Arrays.asList(
+				new Topic("JAVA","JAVA_NAME","JAVA_DESC"),
+				new Topic("SPRING","SPRING_NAME","SPRING_DESC"),
+				new Topic("CPP","CPP_NAME","CPP_DESC")
+				);
+	}
+}
