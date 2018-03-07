47a4cc3d376395ac34832091b090a288efc9e61d
Your Name <you@example.com>
2018-03-07 10:21:29 +0800

    添加put方法添加对象,和postman验证的办法

--- src/main/java/org/bwgl/bootmanager/topic/TopicController.java
+++ src/main/java/org/bwgl/bootmanager/topic/TopicController.java
@@ -5,8 +5,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class TopicController {
@@ -29,5 +32,17 @@ public class TopicController {
 	@RequestMapping("/topic/{id}")
 	public Topic getTopic(@PathVariable String id){
 		return topicService.getTopic(id);
+	}
+	
+	/**
+	 * http://localhost:8080/topics
+	 * {"id":"PYTHON", "name":"PYTHON_NAME", "description":"PYTHON_DESC"}
+	 * PUT
+	 * Content-Type   :   application/json
+	 * @param topic
+	 */
+	@RequestMapping(value="/topics",method=RequestMethod.PUT)
+	public void addTopic(@RequestBody Topic topic){
+		topicService.addTopic(topic); 
 	}
 }
--- src/main/java/org/bwgl/bootmanager/topic/TopicService.java
+++ src/main/java/org/bwgl/bootmanager/topic/TopicService.java
@@ -1,17 +1,20 @@
 package org.bwgl.bootmanager.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class TopicService {
 
-	List<Topic> topics = Arrays.asList(
+	List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic("JAVA", "JAVA_NAME", "JAVA_DESC"),
 			new Topic("SPRING", "SPRING_NAME", "SPRING_DESC"),
 			new Topic("CPP", "CPP_NAME", "CPP_DESC")
+			)
 			);
 
 	public List<Topic> getAllTopics() {
@@ -20,6 +23,10 @@ public class TopicService {
 
 	public Topic getTopic(String id) {
 		return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
 	}
 
 }
