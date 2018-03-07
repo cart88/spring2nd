10c15a646a38f5957fb0368bd5a0611543868a44
Your Name <you@example.com>
2018-03-07 13:55:01 +0800

    添加jpa spring data查询生成方法

--- src/main/java/org/bwgl/bootmanager/course/CourseService.java
+++ src/main/java/org/bwgl/bootmanager/course/CourseService.java
@@ -16,9 +16,38 @@ public class CourseService {
 		courseRepository.findByTopicId(topicid).forEach(courses::add);
 		return courses;
 	}
+	
+	/**
+	 * And	findByNameAndPwd	where name= ? and pwd =?
+	 * Or	findByNameOrSex	where name= ? or sex=?
+	 * Is,Equals	findById,findByIdEquals	where id= ?
+	 * Between	findByIdBetween	where id between ? and ?
+	 * LessThan	findByIdLessThan	where id < ?
+	 * LessThanEquals	findByIdLessThanEquals	where id <= ?
+	 * GreaterThan	findByIdGreaterThan	where id > ?
+	 * GreaterThanEquals	findByIdGreaterThanEquals	where id > = ?
+	 * After	findByIdAfter	where id > ?
+	 * Before	findByIdBefore	where id < ?
+	 * IsNull	findByNameIsNull	where name is null
+	 * isNotNull,NotNull	findByNameNotNull	where name is not null
+	 * Like	findByNameLike	where name like ?
+	 * NotLike	findByNameNotLike	where name not like ?
+	 * StartingWith   findByNameStartingWith	where name like '?%'
+	 * EndingWith	findByNameEndingWith	where name like '%?'
+	 * Containing	findByNameContaining	where name like '%?%'
+	 * OrderBy	findByIdOrderByXDesc	where id=? order by x desc
+	 * Not	findByNameNot	where name <> ?
+	 * In	findByIdIn(Collection<?> c)	where id in (?)
+	 * NotIn	findByIdNotIn(Collection<?> c)	where id not  in (?)
+	 * True	findByAaaTue  where aaa = true
+	 * False	findByAaaFalse	where aaa = false
+	 * IgnoreCase	findByNameIgnoreCase	where UPPER(name)=UPPER(?)
+	 * @param topicid
+	 * @param courseid
+	 * @return
+	 */
 	public Course getCourse(String topicid, String courseid) {
 		return courseRepository.findByIdAndTopicId(courseid,topicid);
-//		return courseRepository.findOne(courseid);
 	}
 	public void addCourse(Course course) {
 		courseRepository.save(course);
